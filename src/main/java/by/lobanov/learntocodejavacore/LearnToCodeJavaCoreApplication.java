88105783d356f8ba82e5eba5f7d22d07c38f0522
Антон Лобанов <ant.lobanov@mail.astondevs.ru>
2025-04-25 13:04:12 +0300

    feat(): initial commit

---
+++ src/main/java/by/lobanov/learntocodejavacore/LearnToCodeJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package by.lobanov.learntocodejavacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToCodeJavaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnToCodeJavaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/lobanov/learntocodejavacore/LearnToCodeJavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lobanov.learntocodejavacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnToCodeJavaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
